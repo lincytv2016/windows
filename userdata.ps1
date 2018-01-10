3cd016ed58d6c57be79784c054d7870da1a83fdf
Lincy Varghese <lincy.varghese@reancloud.com>
2018-01-11 02:33:42 +0530

    swarm cluster added

--- userdata.ps1
+++ userdata.ps1
@@ -1,4 +1,5 @@
 <powershell>
 $admin = [adsi]("WinNT://./administrator, user")
 $admin.psbase.invoke("SetPassword", "Zapcom99")
+docker swarm join --token SWMTKN-1-2ogh9j6o57xen95cg857kjkgzw6z5fiarp5er3nwkqrs5hm5oi-0o9clfe5tiaa6f6lk1xuzh3dh 192.168.3.254:2377
 </powershell>
