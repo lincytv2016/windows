bbdb8d4c1cf31b42fb1be5efcf6eaa3f1900379d
Lincy Varghese <lincy.varghese@reancloud.com>
2018-01-14 03:18:53 +0530

    Update userdata.ps1

--- userdata.ps1
+++ userdata.ps1
@@ -1,6 +1,8 @@
 <powershell>
 $admin = [adsi]("WinNT://./administrator, user")
 $admin.psbase.invoke("SetPassword", "Zapcom99")
+Install-Module DockerProvider -Force
+Install-Package Docker -ProviderName DockerProvider -Force
 Start-Service docker
-docker swarm join --token SWMTKN-1-6bp0mpor8u7lpmqk91sfncdq0sd270dnrnns1col6hbx5ftjm2-c1xtofgrknyirvxux6k04veoy 192.168.3.59:2377
+docker swarm join --token SWMTKN-1-22t1bdzt5rjxwvmkmbi715rafecmee6a0sqxwtrrtymttx8kub-e060vpckf3b5xug13fkufrfjb 192.168.3.31:2377
 </powershell>
