83f51d92879c6396e619f91d7fe3027ae0b80e24
Lincy Varghese <lincy.varghese@reancloud.com>
2018-01-14 00:14:02 +0530

    updated with worker node

--- userdata.ps1
+++ userdata.ps1
@@ -1,5 +1,5 @@
 <powershell>
 $admin = [adsi]("WinNT://./administrator, user")
 $admin.psbase.invoke("SetPassword", "Zapcom99")
-docker swarm join --token SWMTKN-1-2ogh9j6o57xen95cg857kjkgzw6z5fiarp5er3nwkqrs5hm5oi-0o9clfe5tiaa6f6lk1xuzh3dh 192.168.3.254:2377
+docker swarm join --token SWMTKN-1-6bp0mpor8u7lpmqk91sfncdq0sd270dnrnns1col6hbx5ftjm2-c1xtofgrknyirvxux6k04veoy 192.168.3.59:2377
 </powershell>
